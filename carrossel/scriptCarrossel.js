7254b62e2c2aa34a9ab73cab9ee1c4d952c8c733
Leonardo Lima <leonardolb_09@hotmail.com>
2020-10-31 18:40:37 -0300

    Header and a little bit of the main, strikes that will be shown in the index

--- carrossel/scriptCarrossel.js
+++ carrossel/scriptCarrossel.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    
+    //CARROSEL DE IMAGENS Projetos
+    $('.carrosel').slick({
+        dots: true,
+        infinite: true,
+        adaptiveHeight: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        cssEase: 'linear',
+        
+        
+    });
 })
